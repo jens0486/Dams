a4130f4178d3dad154f677f12e01aa06dfa61242
jens0486 <jens0486@web.de>
2011-06-08 16:38:19 +0200

    aktuelle user.php hinzugefügt

---
+++ users.php
@@ -0,0 +1,22 @@
+<?php die("bye"); ?>
+<profiles version='1'>
+admin;1;;;;;;true;
+s_klasen;1;;admin;;;;true;
+d_franke;1;;;;;;true;
+j_seidemann;1;;admin;;;;;
+t_streubel;3;view:2|;;;;;true;
+t_mende;2;;;;;;true;
+w_bornschein;1;;;;;;true;
+backup;5;;;;;;true;
+</profiles>
+<users version='4'>
+admin;admin;21232f297a57a5a743894a0e4a801fc3;;;;;;;
+s_klasen;s_klasen;cc03e747a6afbbcbf8be7668acfebee5;admin;;;;;;
+d_franke;d_franke;d2ad0900de15b104327aebc615d5ce46;admin;;;;;;
+j_seidemann;j_seidemann;94cd7286a957d80f5ea57e65eb6c8730;s_klasen;;;;;;
+t_streubel;t_streubel;d41d8cd98f00b204e9800998ecf8427e;s_klasen;;;;;;
+t_mende;t_mende;45b6aaffbf0a99e81caa22fa2f3bc090;j_seidemann;;;;;;
+w_bornschein;w_bornschein;45b6aaffbf0a99e81caa22fa2f3bc090;j_seidemann;;;;;;
+backup;backup;402051f4be0cc3aad33bcf3ac3d6532b;admin;;;;;;
+login;j_seidemann;098f6bcd4621d373cade4e832627b4f6;j_seidemann;last;first;tsp;phone;mobile;email
+</users>
